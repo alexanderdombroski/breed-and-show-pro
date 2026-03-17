09a3eacaac26d084da2ffc348278b8c0268f4920
Sara Leal Silverio <saralealsilverio@gmail.com>
2026-03-17 11:01:41 -0600

    breeder home and shared layout

---
+++ packages/shared/components/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+  import UserMenu from "./UserMenu.svelte";  
+</script>
+
+    <header class="divider">
+      <div class="logo">
+        <img src="/" alt="Breed & Show logo" />
+        <a href="/">Breed & Show <span class="highlight">Pro</span></a>
+      </div>
+      <nav class="menu-right">
+        <div class="user-menu-container">
+          <UserMenu />
+        </div>
+      </nav>
+    </header>
+    
+<style>
+  
+</style>
