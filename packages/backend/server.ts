a0d73de4c8604fa2f6d0427bf60637f11e879d2b
Alex Dombroski <alexdombroski1@gmail.com>
2026-03-24 10:27:40 -0600

    fix: don't load env file twice

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -1,11 +1,5 @@
 import express from "express";
-import { loadEnvFile } from "node:process";
-import path from "node:path";
 import { router } from "./src/routes/index.ts";
-
-if (process.env.NODE_ENV === "development") {
-  loadEnvFile(path.join(import.meta.dirname, ".env"));
-}
 
 const app = express();
 const PORT = process.env.PORT || 3000;
