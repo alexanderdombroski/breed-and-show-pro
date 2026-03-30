9e39dcd6b465e66faf24b0c87f39ef66660e1748
Alex Dombroski <alexdombroski1@gmail.com>
2026-03-30 11:13:38 -0600

    fix: use sire in litter schema

--- packages/backend/src/db/data/litters.ts
+++ packages/backend/src/db/data/litters.ts
@@ -1,24 +1,9 @@
-import z from "zod";
-
-export const litterSchema = z.object({
-  _id: z.string(),
-  userId: z.string(),
-  sowId: z.string(),
-  boarId: z.string().optional(),
-  totalBorn: z.number().int().nonnegative().optional(),
-  bornAlive: z.number().int().nonnegative().optional(),
-  stillborn: z.number().int().nonnegative().default(0),
-  pigletsWeaned: z.number().int().nonnegative().optional(),
-  pigletIds: z.array(z.string()).default([]),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
-export type Litter = z.infer<typeof litterSchema>;
+import { type Litter, litterSchema } from "../../../../shared/types/schemas.ts";
 
 const _litters: Omit<Litter, "_id" | "userId" | "createdAt" | "updatedAt">[] = [
   {
     sowId: "0", // Ruby
-    boarId: "2", // Titan
+    sireId: "2", // Titan
     totalBorn: 9,
     bornAlive: 8,
     stillborn: 1,
@@ -26,7 +11,7 @@ const _litters: Omit<Litter, "_id" | "userId" | "createdAt" | "updatedAt">[] = [
   },
   {
     sowId: "1", // Daisy
-    boarId: "3", // Crusher
+    sireId: "3", // Crusher
     totalBorn: 3,
     bornAlive: 3,
     stillborn: 0,
--- packages/shared/types/schemas.ts
+++ packages/shared/types/schemas.ts
@@ -1,32 +1,6 @@
 import { z } from "zod";
 
-// -------------------- DATE RECORDS --------------------
-
-// Used for future events. Events can be converted to notes
-export const BaseDateRecordSchema = z.object({
-  _id: z.string(),
-  date: z.date(),
-  isEstimated: z.boolean().default(false),
-  label: z.string().optional(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
-
-export const PigDateRecordSchema = BaseDateRecordSchema.extend({
-  pigId: z.string(),
-  _type: z.enum(["farrow", "heat", "breed", "vaccination"]),
-});
-type PigDateRecord = z.infer<typeof PigDateRecordSchema>;
-
-export const LitterDateRecordSchema = BaseDateRecordSchema.extend({
-  litterId: z.string(),
-  _type: z.enum(["weaning"]),
-});
-type LitterDateRecord = z.infer<typeof LitterDateRecordSchema>;
-
-export type DateRecord = PigDateRecord | LitterDateRecord;
-
-// -------------------- PIGS --------------------
+// -------------------- PIGS COLLECTION --------------------
 
 export const pigSchema = z.object({
   _id: z.string(),
@@ -34,7 +8,7 @@ export const pigSchema = z.object({
   name: z.string(),
   earNotch: z.string(),
   breed: z.string(),
-  sex: z.enum(["sow", "boar", "gilt", "barrow"]),
+  sex: z.enum(["sow", "boar", "gilt", "barrow", "sire"]),
   status: z.enum(["open", "bred", "farrowed", "active"]),
   birthDate: z.date(),
   show: z.object({
@@ -47,12 +21,12 @@ export const pigSchema = z.object({
 });
 export type Pig = z.infer<typeof pigSchema>;
 
-// -------------------- LITTER STATS --------------------
+// -------------------- LITTER COLLECTION --------------------
 export const litterSchema = z.object({
   _id: z.string(),
   userId: z.string(),
   sowId: z.string(),
-  boarId: z.string().optional(),
+  sireId: z.string().optional(),
   totalBorn: z.number().int().nonnegative(),
   bornAlive: z.number().int().nonnegative().optional(),
   stillborn: z.number().int().nonnegative().optional(),
@@ -62,7 +36,7 @@ export const litterSchema = z.object({
 });
 export type Litter = z.infer<typeof litterSchema>;
 
-// -------------------- USERS --------------------
+// -------------------- USERS COLLECTION --------------------
 
 export const userSchema = z.object({
   _id: z.string(),
@@ -72,7 +46,33 @@ export const userSchema = z.object({
 });
 export type User = z.infer<typeof userSchema>;
 
-// -------------------- NOTES --------------------
+// -------------------- EVENTS COLLECTION --------------------
+
+// Used for future events. Events can be converted to notes
+export const BaseEventSchema = z.object({
+  _id: z.string(),
+  date: z.date(),
+  isEstimated: z.boolean().default(false),
+  label: z.string().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export const PigEventSchema = BaseEventSchema.extend({
+  pigId: z.string(),
+  _type: z.enum(["farrow", "heat", "breed", "vaccination"]),
+});
+type PigEvent = z.infer<typeof PigEventSchema>;
+
+export const LitterEventSchema = BaseEventSchema.extend({
+  litterId: z.string(),
+  _type: z.enum(["weaning"]),
+});
+type LitterEvent = z.infer<typeof LitterEventSchema>;
+
+export type Event = PigEvent | LitterEvent;
+
+// -------------------- NOTES COLLECTION --------------------
 
 export const NoteSchema = z.object({
   _id: z.string(),
