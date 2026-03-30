edcd5627ed3f0c0b2594711a00c2b4f05e86b9d8
Alex Dombroski <alexdombroski1@gmail.com>
2026-03-30 10:23:50 -0600

    fix: type checks

--- packages/backend/src/db/data/users.ts
+++ packages/backend/src/db/data/users.ts
@@ -1,20 +1,23 @@
-import { userSchema, type User } from "../../../../shared/js/types.ts";
+import { userSchema, type User } from "../../../../shared/types/schemas.ts";
 
 const _users: User[] = [
   {
     _id: "1",
     name: "Sue Barker",
     email: "sueb@gmail.com",
+    farmName: "Wild West Farm",
   },
   {
     _id: "2",
     name: "Freddy Smith",
     email: "freds123@gmail.com",
+    farmName: "Green Acres Farm",
   },
   {
     _id: "3",
     name: "Mitch Chopsworth",
     email: "mitchmitch67@gmail.com",
+    farmName: "Grandpa's Pigs",
   },
 ];
 
