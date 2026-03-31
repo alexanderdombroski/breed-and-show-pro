b5ea6561d684e0f7dffa098365b08ce9c8b0dfc5
Alex Dombroski <alexdombroski1@gmail.com>
2026-03-31 10:13:27 -0600

    chore: merge conflicts

--- packages/backend/src/db/data/users.ts
+++ packages/backend/src/db/data/users.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { userSchema, type User } from "../../../../shared/types/schemas.ts";
-=======
-import { userSchema, type User } from "../../../../shared/js/types.ts";
->>>>>>> 4d9bf48 (mongodb and routes setup)
 
 const _users: User[] = [
   {
