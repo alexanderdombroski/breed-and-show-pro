2f34f26676464b4c30826d2ea7dd93e2260fc0d8
Alex Dombroski <alexdombroski1@gmail.com>
2026-04-06 06:58:43 -0600

    feat: add edit button

--- packages/exhibitor/components/PigShow.svelte
+++ packages/exhibitor/components/PigShow.svelte
@@ -7,21 +7,34 @@
   const { pig }: Props = $props();
 </script>
 
-{#if pig.show}
-  <div class="show-info">
-    <span class="show-label">Upcoming Show:</span>
-    <p class="show-name">{pig.show.name ?? "Not Set"}</p>
-    {#if pig.show.date}
-      <p class="show-date">
-        {new Date(pig.show.date).toLocaleDateString()}
-      </p>
-    {/if}
-  </div>
-{/if}
+<div class="show-info">
+  <span class="show-label">Upcoming Show:</span>
+  <button aria-label="edit" class="edit">
+    <svg
+      role="presentation"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      ><g data-name="Layer 2"
+        ><g data-name="edit"
+          ><rect width="24" height="24" opacity="0" /><path
+            d="M19.4 7.34L16.66 4.6A2 2 0 0 0 14 4.53l-9 9a2 2 0 0 0-.57 1.21L4 18.91a1 1 0 0 0 .29.8A1 1 0 0 0 5 20h.09l4.17-.38a2 2 0 0 0 1.21-.57l9-9a1.92 1.92 0 0 0-.07-2.71zM9.08 17.62l-3 .28.27-3L12 9.32l2.7 2.7zM16 10.68L13.32 8l1.95-2L18 8.73z"
+          /></g
+        ></g
+      ></svg
+    >
+  </button>
+  <p class="show-name">{pig.show?.name ?? "Not Set"}</p>
+  {#if pig.show?.date}
+    <p class="show-date">
+      {new Date(pig.show.date).toLocaleDateString()}
+    </p>
+  {/if}
+</div>
 
 <style>
   /* Show Info Section */
   .show-info {
+    position: relative;
     margin-top: auto; /* Pushes show info to the bottom */
     background: #f9fafb;
     padding: 0.75rem;
@@ -34,6 +47,27 @@
     text-transform: uppercase;
     letter-spacing: 0.05em;
     color: #9ca3af;
+  }
+
+  .edit {
+    position: absolute;
+    top: 0.25rem;
+    right: 0.25rem;
+    background-color: transparent;
+    border: none;
+    color: black;
+  }
+
+  .edit svg {
+    color: black;
+    fill: black;
+    width: 2rem;
+    height: 2rem;
+  }
+
+  .edit svg:hover {
+    fill: #f2af29ff;
+    cursor: pointer;
   }
 
   .show-name {
