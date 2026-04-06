07c987405ec07a084d4066c38bc27be193240018
Alex Dombroski <alexdombroski1@gmail.com>
2026-04-06 07:13:40 -0600

    feat: separate pig link and edit button

--- packages/exhibitor/components/PigShow.svelte
+++ packages/exhibitor/components/PigShow.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import { Popover } from "bits-ui";
   import type { Pig } from "../../shared/types/schemas.ts";
+
   type Props = {
     pig: Pick<Pig, "_id" | "show">;
   };
@@ -9,20 +11,28 @@
 
 <div class="show-info">
   <span class="show-label">Upcoming Show:</span>
-  <button aria-label="edit" class="edit">
-    <svg
-      role="presentation"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      ><g data-name="Layer 2"
-        ><g data-name="edit"
-          ><rect width="24" height="24" opacity="0" /><path
-            d="M19.4 7.34L16.66 4.6A2 2 0 0 0 14 4.53l-9 9a2 2 0 0 0-.57 1.21L4 18.91a1 1 0 0 0 .29.8A1 1 0 0 0 5 20h.09l4.17-.38a2 2 0 0 0 1.21-.57l9-9a1.92 1.92 0 0 0-.07-2.71zM9.08 17.62l-3 .28.27-3L12 9.32l2.7 2.7zM16 10.68L13.32 8l1.95-2L18 8.73z"
-          /></g
-        ></g
-      ></svg
+  <Popover.Root>
+    <Popover.Trigger
+      aria-label="edit"
+      class="edit"
     >
-  </button>
+      <svg
+        role="presentation"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        ><g data-name="Layer 2"
+          ><g data-name="edit"
+            ><rect width="24" height="24" opacity="0" /><path
+              d="M19.4 7.34L16.66 4.6A2 2 0 0 0 14 4.53l-9 9a2 2 0 0 0-.57 1.21L4 18.91a1 1 0 0 0 .29.8A1 1 0 0 0 5 20h.09l4.17-.38a2 2 0 0 0 1.21-.57l9-9a1.92 1.92 0 0 0-.07-2.71zM9.08 17.62l-3 .28.27-3L12 9.32l2.7 2.7zM16 10.68L13.32 8l1.95-2L18 8.73z"
+            /></g
+          ></g
+        ></svg
+      >
+    </Popover.Trigger>
+    <Popover.Content>
+      <p>Hello World</p>
+    </Popover.Content>
+  </Popover.Root>
   <p class="show-name">{pig.show?.name ?? "Not Set"}</p>
   {#if pig.show?.date}
     <p class="show-date">
@@ -49,25 +59,24 @@
     color: #9ca3af;
   }
 
-  .edit {
+  :global(.edit) {
     position: absolute;
     top: 0.25rem;
     right: 0.25rem;
     background-color: transparent;
     border: none;
     color: black;
-  }
 
-  .edit svg {
-    color: black;
-    fill: black;
-    width: 2rem;
-    height: 2rem;
-  }
+    & svg {
+      fill: black;
+      width: 2rem;
+      height: 2rem;
 
-  .edit svg:hover {
-    fill: #f2af29ff;
-    cursor: pointer;
+      &:hover {
+        fill: #f2af29ff;
+        cursor: pointer;
+      }
+    }
   }
 
   .show-name {
