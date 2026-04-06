0cd25ae1586af3ce0fa506da9fa3d4fec151e9c7
Alex Dombroski <alexdombroski1@gmail.com>
2026-04-06 08:27:47 -0600

    feat(dev): add automated svlete formatting

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,7 @@
 const joinFiles = (files) => files.map((file) => `"${file}"`).join(" ");
 
 module.exports = {
-  "*.{ts,js,mjs,cjs,mts,astro}": (stagedFiles) => {
+  "*.{ts,js,mjs,cjs,mts,astro,svelte}": (stagedFiles) => {
     const files = joinFiles(stagedFiles);
     return [`prettier --write ${files}`, `eslint --max-warnings=0 ${files}`];
   },
