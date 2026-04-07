33f4068de9adbaeffc806d9613864cfccf7c63c3
Alex Dombroski <alexdombroski1@gmail.com>
2026-04-07 08:03:01 -0600

    fix: allow astro origin on server in dev

--- packages/backend/src/middleware/index.ts
+++ packages/backend/src/middleware/index.ts
@@ -1,17 +1,22 @@
 export * from "./cors.ts";
 export * from "./logging.ts";
 import cors from "cors";
+import { isDev } from "../utils/index.ts";
 
 // This is the "VIP List" that the browser checks
 const allowedOrigins = [
   "https://alexanderdombroski.github.io", // Original GH Pages
-  "http://alexdombroski.com", // Your new custom domain
   "https://alexdombroski.com", // Your domain with SSL
-  "http://www.alexdombroski.com", // www version
   "https://www.alexdombroski.com", // www with SSL
-  "http://localhost:4321", // Local Astro dev
-  "http://localhost:3000", // Local Server dev
 ];
+
+if (isDev) {
+  allowedOrigins.push(
+    "http://localhost:4322", // Local Astro dev
+    "http://localhost:4321", // Local Astro dev
+    "http://localhost:3000", // Local Server dev
+  );
+}
 
 export const corsHandler = cors({
   origin: (origin, callback) => {
